619d39b2fc31146c292aa0e539f1afefc128eb72
Sharath G <sharathgoud@live.com>
2019-09-26 21:47:21 -0500

    Updates

---
+++ precache-manifest.3d1c3691214fc9048344f4e7f4e9e730.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8339811be41dcb7f0f40ff3186ef4919",
+    "url": "/react-confusion/index.html"
+  },
+  {
+    "revision": "f426e52fa4dcf6c74262",
+    "url": "/react-confusion/static/css/2.7199defb.chunk.css"
+  },
+  {
+    "revision": "9649efcf466a7723ac50",
+    "url": "/react-confusion/static/css/main.3e871323.chunk.css"
+  },
+  {
+    "revision": "f426e52fa4dcf6c74262",
+    "url": "/react-confusion/static/js/2.a7ef3e48.chunk.js"
+  },
+  {
+    "revision": "9649efcf466a7723ac50",
+    "url": "/react-confusion/static/js/main.2cfb011f.chunk.js"
+  },
+  {
+    "revision": "e81e148d6847adff814f",
+    "url": "/react-confusion/static/js/runtime~main.d5d0b6a7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
